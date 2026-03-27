3c00d4a97d0d9ab29c5eb1163a0f654e7ad4bcf9
Gostev, Philipp <philipp.gostev@adesso.de>
2026-03-27 10:14:45 +0100

    initial backend

--- backend/src/main/java/org/constructx/policyhub/PolicyHubApplication.java
+++ backend/src/main/java/org/constructx/policyhub/PolicyHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class PolicyHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(PolicyHubApplication.class, args);
 	}
 
 }
